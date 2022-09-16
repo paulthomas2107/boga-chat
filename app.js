ea86bd9d0ccf2b81924b3f32a9d3e71cca90a995
Paul Thomas <paulthomas2107@gmail.com>
2022-09-16 22:07:31 +0100

    More.....

--- app.js
+++ app.js
@@ -5,11 +5,38 @@ supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3eGhlZW9jbmtlZXRjdG14Y21wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzNDY5NDEsImV4cCI6MTk3ODkyMjk0MX0.UE_Q7SZScwN0jhSShJKXj6yNMODynHBniOsPXyeXrlQ'
 );
 
-async function getMessages() {
-  let { data, error } = await supabase.from('messages').select('*');
-  return data;
+const messagesElement = document.querySelector('#messages');
+
+function addMessageToPage(message) {
+  const element = document.createElement('li');
+  element.classList.add('card', 'm-2');
+  element.innerHTML = `
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-2 avatar-container">
+                <img src="monkey3.png" width="100" height="100" />
+                <p class="avatar-username">${message.username}</p>
+              </div>
+              <div class="col-sm-10">
+                <p>${message.content}</p>
+              </div>
+            </div>
+            <div class="row">
+              <p class="col-sm-12 timestamp">${message.created_at}</p>
+            </div>
+          </div>
+        `;
+  messagesElement.append(element);
 }
 
-getMessages().then((data) => {
+async function init() {
+  let { data, error } = await supabase.from('messages').select('*');
+
   console.log(data);
-});
+
+  data.forEach((element) => {
+    addMessageToPage(element);
+  });
+}
+
+init();
---
+++ scratch.js
@@ -0,0 +1,27 @@
+<li class="card m-2">
+  <div class="card-body">
+    <div class="row">
+      <div class="col-sm-2 avatar-container">
+        <img src="monkey3.png" width="100" height="100" />
+        <p class="avatar-username">LightSleeper</p>
+      </div>
+      <div class="col-sm-10">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius soluta,
+          fugiat sunt nisi laudantium perspiciatis nemo pariatur ab
+          reprehenderit quo placeat dolore tempora enim velit quod ut aperiam
+          exercitationem doloremque!Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Eius soluta, fugiat sunt nisi laudantium
+          perspiciatis nemo pariatur ab reprehenderit quo placeat dolore tempora
+          enim velit quod ut aperiam exercitationem doloremque!Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Eius soluta, fugiat sunt nisi
+          laudantium perspiciatis nemo pariatur ab reprehenderit quo placeat
+          dolore tempora enim velit quod ut aperiam exercitationem doloremque!
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <p class="col-sm-12 timestamp">4:20PM Today</p>
+    </div>
+  </div>
+</li>;
